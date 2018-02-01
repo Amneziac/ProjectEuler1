bb37767bc768925280504c9111a5a73d399d7cb0
Amneziac <Dmars8047@live.com>
2018-02-01 09:19:03 -0500

    Add project files.

---
+++ ProjectEuler1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> multiples = new List<int>();
+            int total = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 | i % 5 == 0)
+                    multiples.Add(i);
+            }
+
+            foreach (int m in multiples)
+            {
+                total += m;
+            }
+
+            Console.Write(total);
+        }
+    }
+}
